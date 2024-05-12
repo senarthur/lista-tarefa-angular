d978417c44bd2737408d1f8ba8b043ade7c5b568
Arthur Sena <sena.arthur698@gmail.com>
2024-05-12 16:08:15 -0300

    Created search bar styling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { TarefaService } from '../../services/tarefa.service';
+import { ITask, createNewTask } from '../../models/ITarefa.model';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  constructor(private tarefaService: TarefaService) {}
+  
 }
